367d2f99e25546571df081e9f5a6eecebaaa82bd
Erik19852404 <yorick_1985@mail.ru>
2020-04-16 17:07:27 +0600

    2 files modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -1,7 +1,7 @@
 export const teamMembers = [
   {
     name: 'tester',
-    photo: 'assets/photo/photo_1.png',
+    photo: 'https://cdn.jsdelivr.net/gh/Erik19852404/zya@master/src/photo/IMG_20190616_170835.jpg',
     photo_description: 'photo_1',
     about: 'text',
   },
