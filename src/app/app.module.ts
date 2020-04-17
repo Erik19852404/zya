baa9f7fd385cf38c4b9d72646771b78dfe327ed0
Erik19852404 <yorick_1985@mail.ru>
2020-04-17 10:43:57 +0600

    8 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -8,10 +9,17 @@ import { MenuComponent } from './menu/menu.component';
 import { InformationComponent } from './information/information.component';
 import { FooterComponent } from './footer/footer.component';
 import { OurTeamComponent } from './our-team/our-team.component';
+import { DetailesComponent } from './detailes/detailes.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TopBarComponent, MenuComponent, InformationComponent, FooterComponent, OurTeamComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot(
+    [
+      { path:"", component: InformationComponent },
+      { path:"detailes", component: DetailesComponent }
+    ]
+  ), ],
+  declarations: [ AppComponent, TopBarComponent, MenuComponent, InformationComponent, FooterComponent, OurTeamComponent, DetailesComponent ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
