dc53d1e7a19282cf076a2a2bea930ca35e343674
Erik19852404 <yorick_1985@mail.ru>
2020-04-17 11:02:40 +0600

    5 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,22 @@ import { InformationComponent } from './information/information.component';
 import { FooterComponent } from './footer/footer.component';
 import { OurTeamComponent } from './our-team/our-team.component';
 import { DetailesComponent } from './detailes/detailes.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SevicesComponent } from './sevices/sevices.component';
+import { LicensesComponent } from './licenses/licenses.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
   RouterModule.forRoot(
     [
       { path:"", component: InformationComponent },
-      { path:"detailes", component: DetailesComponent }
+      { path:"detailes", component: DetailesComponent },
+      { path:"licenses", component: LicensesComponent },
+      { path:"contacts", component: ContactsComponent },
+      { path:"services", component: SevicesComponent },
     ]
   ), ],
-  declarations: [ AppComponent, TopBarComponent, MenuComponent, InformationComponent, FooterComponent, OurTeamComponent, DetailesComponent ],
+  declarations: [ AppComponent, TopBarComponent, MenuComponent, InformationComponent, FooterComponent, OurTeamComponent, DetailesComponent, ContactsComponent, SevicesComponent, LicensesComponent ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
