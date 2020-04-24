46c1a2a73de07a0b9671b3fcab02878e6b5e95a5
Erik19852404 <yorick_1985@mail.ru>
2020-04-24 14:56:19 +0600

    3 files modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -1,8 +1,8 @@
 export const teamMembers = [
   {
-    name: 'tester',
+    name: 'Ногай Зоя Романовна',
     photo: 'https://i.pinimg.com/236x/04/48/60/044860ebcd5d6c14a1140b351cb620b1--simpsons-marge-simpsons-characters.jpg',
     photo_description: 'photo_1',
-    about: 'text',
+    about: 'Тут будет краткая информация.',
   },
 ];
